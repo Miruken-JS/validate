3bccf1fd76760c8921ed76697b5a48900794b875
Craig Neuwirt <cneuwirt@gmail.com>
2020-05-05 16:41:40 -0500

    Fixed ArrayManager

--- config.js
+++ config.js
@@ -8,9 +8,9 @@ System.config({
 
   map: {
     "chai": "npm:chai@3.5.0",
-    "miruken-callback": "npm:miruken-callback@0.0.5-alpha.41",
-    "miruken-context": "npm:miruken-context@0.0.5-alpha.20",
-    "miruken-core": "npm:miruken-core@0.0.5-alpha.16",
+    "miruken-callback": "npm:miruken-callback@0.0.5-alpha.42",
+    "miruken-context": "npm:miruken-context@0.0.5-alpha.21",
+    "miruken-core": "npm:miruken-core@0.0.5-alpha.17",
     "validate.js": "npm:validate.js@0.13.1",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.5.0"
@@ -53,14 +53,14 @@ System.config({
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
-    "npm:miruken-callback@0.0.5-alpha.41": {
-      "miruken-core": "npm:miruken-core@0.0.5-alpha.16"
+    "npm:miruken-callback@0.0.5-alpha.42": {
+      "miruken-core": "npm:miruken-core@0.0.5-alpha.17"
     },
-    "npm:miruken-context@0.0.5-alpha.20": {
-      "miruken-callback": "npm:miruken-callback@0.0.5-alpha.41",
-      "miruken-core": "npm:miruken-core@0.0.5-alpha.16"
+    "npm:miruken-context@0.0.5-alpha.21": {
+      "miruken-callback": "npm:miruken-callback@0.0.5-alpha.42",
+      "miruken-core": "npm:miruken-core@0.0.5-alpha.17"
     },
-    "npm:miruken-core@0.0.5-alpha.16": {
+    "npm:miruken-core@0.0.5-alpha.17": {
       "reflect-metadata": "npm:reflect-metadata@0.1.13"
     },
     "npm:process@0.11.10": {
